623da81d5343215cceae238217679b115c0b5973
hduser@alecfei-linuxlaptop <2022173@student.cct.ie>
2024-07-07 19:02:02 +0100

    data cleaning

--- data cleaning and transformation.R
+++ data cleaning and transformation.R
@@ -1,9 +1,24 @@
-# Suppress warnings
-suppressMessages(tidyverse_conflicts)
-
 # Import library
 library(tidyverse)
 
-# Read data
+# Suppress warnings
+suppressMessages(tidyverse_conflicts)
+
+# Read bike station data
 divvy_station_2013 <- read_csv('/home/hduser/projects for portfolios/divvy_data/Divvy_Stations_2013.csv')
+spec(divvy_station_2013)
 head(divvy_station_2013)
+str(divvy_station_2013)
+summary(divvy_station_2013)
+View(divvy_station_2013)
+
+divvy_station_2017_2 <- read_csv('/home/hduser/projects for portfolios/divvy_data/Divvy_Stations_2017_Q3Q4.csv')
+View(divvy_station_2017_2)
+spec(divvy_station_2017_2)
+
+# Read bike trip data
+divvy_trip_2013 <- read_csv('/home/hduser/projects for portfolios/divvy_data/Divvy_Trips_2013.csv')
+spec(divvy_trip_2013)
+summary(divvy_trip_2013)
+View(divvy_trip_2013)
+
