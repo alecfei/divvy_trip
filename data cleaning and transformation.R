3fbcdc838e09beb377fe13a040e93378339aa38b
hduser@alecfei-linuxlaptop <2022173@student.cct.ie>
2024-07-09 16:43:33 +0100

    data analysis using R

--- data cleaning and transformation.R
+++ data cleaning and transformation.R
@@ -1,26 +1,40 @@
+# Suppress warnings
+suppressMessages(library(tidyverse))
+
 # Import library
 library(tidyverse)
 
-# Suppress warnings
-suppressMessages(tidyverse_conflicts)
+# Read latest bike station data
+divvy_station_2017 <- read_csv('/home/hduser/projects for portfolios/divvy_data/Divvy_Stations_2017_Q3Q4.csv')
+View(divvy_station_2017)
+spec(divvy_station_2017) # extracts the full column specification from a tibble (dataframe in Python)
+unique(divvy_station_2017$city)
+n_distinct(divvy_station_2017$city)
+unique(divvy_station_2017$...8)
+divvy_station_2017 <- divvy_station_2017 %>% select(-...8) # drop column
 
-# Read bike station data
-divvy_station_2013 <- read_csv('/home/hduser/projects for portfolios/divvy_data/Divvy_Stations_2013.csv')
-spec(divvy_station_2013)
-head(divvy_station_2013)
-str(divvy_station_2013)
-summary(divvy_station_2013)
-View(divvy_station_2013)
-n_distinct(divvy_station_2013$name)
-unique(divvy_station_2013$name)
+# covert online_date to datetime type
+divvy_station_2017 <- divvy_station_2017 %>%
+  mutate(online_date = mdy_hm(online_date))
 
-divvy_station_2017_2 <- read_csv('/home/hduser/projects for portfolios/divvy_data/Divvy_Stations_2017_Q3Q4.csv')
-View(divvy_station_2017_2)
-spec(divvy_station_2017_2)
-summary(divvy_station_2017_2)
-n_distinct(divvy_station_2017_2$name)
-unique(divvy_station_2017_2$name)
+head(divvy_station_2017)
 
+dim(divvy_station_2017) # dimension of the tibble
+glimpse(divvy_station_2017)
+str(divvy_station_2017)
+summary(divvy_station_2017)
+
+# Detailed summary for character columns
+character_columns <- sapply(divvy_station_2017, is.character)
+lapply(divvy_station_2017[, character_columns], table)
+
+# Groupby date
+station_number_byyear <- divvy_station_2017 %>% 
+  group_by(year(online_date)) %>%
+  summarise(station_count = n()) %>%
+  rename(year = `year(online_date)`)
+
+print(station_number_byyear)
 
 # Read bike trip data
 divvy_trip_2013 <- read_csv('/home/hduser/projects for portfolios/divvy_data/Divvy_Trips_2013.csv')
